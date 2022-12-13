7d5505fb3509e68bd09a6e6cea0a3480fdcc48de
“anna.radova“ <Anna_Radova@epam.com>
2022-12-13 14:12:42 +0200

    Solution for Task 1

---
+++ Homework5/Homework5/Task1.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Homework5
+{
+    public class Task1
+    {
+        public List<int> GetIntegersFromList(List<object> objList)
+        {
+            List<int> intList = objList
+                .Where(i => i.GetType() == typeof(int))
+                .Cast<int>()
+                .ToList();
+            return intList;
+        }
+
+        [Test]
+        public void GetIntegersFromListTest1()
+        {
+            List<object> objList = new List<object>() {1, 2, "a", "b"};
+            List<int> expectedList = new List<int>() {1, 2};
+            List<int> actualList = GetIntegersFromList(objList);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [Test]
+        public void GetIntegersFromListTest2()
+        {
+            List<object> objList = new List<object>() {1, 2, "a", "b", 0, 15};
+            List<int> expectedList = new List<int>() {1, 2, 0, 15};
+            List<int> actualList = GetIntegersFromList(objList);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [Test]
+        public void GetIntegersFromListTest3()
+        {
+            List<object> objList = new List<object>() {1, 2, "a", "b", "aasf", "1", "123", 231};
+            List<int> expectedList = new List<int>() {1, 2, 231};
+            List<int> actualList = GetIntegersFromList(objList);
+            Assert.AreEqual(expectedList, actualList);
+        }
+    }
+}
