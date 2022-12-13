dcf7c9502f9f5cd3bba18501e288bdfc1cf91321
“anna.radova“ <Anna_Radova@epam.com>
2022-12-13 14:15:41 +0200

    Solution for Task 2

---
+++ Homework5/Homework5/Task2.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Homework5
+{
+    public class Task2
+    {
+        public static char FirstNonRepeatingLetter(string inputStr)
+        {
+            int repeatedChar = 0;
+            char[] chars = inputStr.ToCharArray();
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                repeatedChar = 0;
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    if ((i != j) && (chars[i] == chars[j]))
+                    {
+                        repeatedChar++;
+                        break;
+                    }
+                }
+                if (repeatedChar == 0)
+                {
+                    return chars[i];
+                }
+            }
+            return ' ';
+        }
+
+        [Test]
+        public void FirstNonRepeatingLetterTest1()
+        {
+            string inputStr = "stress";
+            char expectedLetter = 't';
+            char actualLetter = FirstNonRepeatingLetter(inputStr);
+            Assert.AreEqual(expectedLetter, actualLetter);
+        }
+
+        [Test]
+        public void FirstNonRepeatingLetterTest2()
+        {
+            string inputStr = "sTress";
+            char expectedLetter = 'T';
+            char actualLetter = FirstNonRepeatingLetter(inputStr);
+            Assert.AreEqual(expectedLetter, actualLetter);
+        }
+
+        [Test]
+        public void FirstNonRepeatingLetterTest3()
+        {
+            string inputStr = "sttrreess";
+            char expectedLetter = ' ';
+            char actualLetter = FirstNonRepeatingLetter(inputStr);
+            Assert.AreEqual(expectedLetter, actualLetter);
+        }
+    }
+}
