e41cf8506d145b03c7b17a8d2532b12060368017
“anna.radova“ <Anna_Radova@epam.com>
2022-12-13 14:20:06 +0200

    Solution for Task 5

---
+++ Homework5/Homework5/Task5.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Homework5
+{
+    public class Task5
+    {
+        public string StringSort(string str)
+        {
+            return "(" + string.Join(")(",
+                    str.
+                    ToUpper().
+                    Split(';').
+                    Select(n => n.Split(':')).
+                    Select(n => string.Join(", ", n.Reverse())).
+                    OrderBy(n => n)
+                ) + ")";
+        }
+
+        [Test]
+        public void StringSortTest1()
+        {
+            string str = "Fred:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+            string expectedStr = "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)";
+            string actualStr = StringSort(str);
+            Assert.AreEqual(expectedStr, actualStr);
+        }
+    }
+}
